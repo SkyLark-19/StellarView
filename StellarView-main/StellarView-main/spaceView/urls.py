e9999294b86b1218df28ab861ecc1d80fe7e3c44
SkyLark-19 <en108923@foe.sjp.ac.lk>
2024-10-06 23:11:55 +0530

    Delete StellarView-main directory

--- StellarView-main/StellarView-main/spaceView/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "spaceView"
-urlpatterns=[
-    path("home/", views.index, name="index"),
-    path("view/", views.view, name="view"),
-]
